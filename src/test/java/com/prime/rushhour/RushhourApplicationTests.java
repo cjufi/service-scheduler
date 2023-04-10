bfbb7cd955be1ac25a7f4c7446573cb7dfba3133
cjufi <ftasic1202@gmail.com>
2023-04-10 14:42:29 +0200

    first commit

---
+++ src/test/java/com/prime/rushhour/RushhourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime.rushhour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushhourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
