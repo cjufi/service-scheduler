f28784f4abaf8118307a582557118461cefd3d1f
cjufi <ftasic1202@gmail.com>
2023-04-11 10:20:38 +0200

    Fix Request Dto

--- src/main/java/com/prime/rushhour/domain/provider/dto/ProviderRequestDto.java
+++ src/main/java/com/prime/rushhour/domain/provider/dto/ProviderRequestDto.java
@@ -1,9 +1,7 @@
 package com.prime.rushhour.domain.provider.dto;
 
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,16 +20,16 @@ public record ProviderRequestDto(
     @NotBlank(message = "Domain is a mandatory field")
     @Column(unique = true)
     @Size(min = 2, message = "Domain must be at least 2 characters long")
-    @Pattern(regexp = "^[a-zA-Z0/9]*$")
+    @Pattern(regexp = "^[a-zA-Z\\d]*$")
     String businessDomain,
     @NotBlank(message = "Phone is a mandatory field")
-    @Pattern(regexp = "^\\+?[0/9]+$")
+    @Pattern(regexp = "^\\+?\\d+$")
     String phone,
-    @NotBlank(message = "Start Time is a mandatory field")
+    @NotNull(message = "Start Time is a mandatory field")
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     LocalTime startTimeOfWorkingDay,
-    @NotBlank(message = "End Time is a mandatory field")
+    @NotNull(message = "End Time is a mandatory field")
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-    @NotBlank(message = "Setting working days is a must")
     LocalTime endTimeOfWorkingDay,
+    @NotNull(message = "Setting working days is a must")
     Set<DayOfWeek> workingDays) { }
