83de5037d7df55211f27e6cd918ad9e3f000188d
cjufi <ftasic1202@gmail.com>
2023-04-11 10:47:39 +0200

    Add flyway migration tool

---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS provider (
+                                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                        name VARCHAR(255) NOT NULL,
+                                        website VARCHAR(255) NOT NULL,
+                                        business_domain VARCHAR(255) NOT NULL,
+                                        phone VARCHAR(255) NOT NULL,
+                                        start_time_of_working_day TIME NOT NULL,
+                                        end_time_of_working_day TIME NOT NULL,
+                                        CONSTRAINT unique_name UNIQUE (name),
+                                        CONSTRAINT unique_business_domain UNIQUE (business_domain)
+);
+
+CREATE TABLE IF NOT EXISTS working_days (
+                                            provider_id BIGINT NOT NULL,
+                                            working_day VARCHAR(255) NOT NULL,
+                                            CONSTRAINT fk_provider_id
+                                                FOREIGN KEY (provider_id)
+                                                REFERENCES provider (id)
+                                                ON DELETE CASCADE
+);
