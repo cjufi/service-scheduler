8d4a0a36576e9276b80d716dacdbf4684c9822c4
cjufi <ftasic1202@gmail.com>
2023-04-24 11:52:31 +0200

    Add Role

--- src/main/resources/db/migration/V2__Add_roles.sql
+++ src/main/resources/db/migration/V2__Add_roles.sql
@@ -1,4 +1,10 @@
-CREATE ROLE ADMIN;
-CREATE ROLE PROVIDER_ADMIN;
-CREATE ROLE EMPLOYEE;
-CREATE ROLE CLIENT;
+CREATE TABLE IF NOT EXISTS role (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    name VARCHAR(255) NOT NULL,
+                                    CONSTRAINT unique_name UNIQUE (name)
+);
+
+INSERT INTO role (name) VALUES ('ADMIN');
+INSERT INTO role (name) VALUES ('PROVIDER_ADMIN');
+INSERT INTO role (name) VALUES ('EMPLOYEE');
+INSERT INTO role (name) VALUES ('CLIENT');
