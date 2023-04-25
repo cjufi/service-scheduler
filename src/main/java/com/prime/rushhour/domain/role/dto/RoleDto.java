b20b4f77bc412f4bb38475e61cf9f9563c4caa10
cjufi <ftasic1202@gmail.com>
2023-04-25 12:47:41 +0200

    Add size validation for Role name

--- src/main/java/com/prime/rushhour/domain/role/dto/RoleDto.java
+++ src/main/java/com/prime/rushhour/domain/role/dto/RoleDto.java
@@ -2,9 +2,11 @@ package com.prime.rushhour.domain.role.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record RoleDto(
         @NotBlank(message = "Role name is a mandatory field")
+        @Size(min = 3, message = "Name must be at least 3 characters long")
         @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Name should be alphanumeric with underscores only")
         String name) {
 }
