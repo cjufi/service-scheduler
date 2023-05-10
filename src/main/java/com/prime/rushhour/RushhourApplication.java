6e9e65032c0441723ed1c74a28d6e7a81b7e022a
cjufi <ftasic1202@gmail.com>
2023-05-10 12:29:17 +0200

    Implement RsaKeyProperties

--- src/main/java/com/prime/rushhour/RushhourApplication.java
+++ src/main/java/com/prime/rushhour/RushhourApplication.java
@@ -1,8 +1,11 @@
 package com.prime.rushhour;
 
+import com.prime.rushhour.infrastructure.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RushhourApplication {
 
