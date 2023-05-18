b70aad650eee2390b9241823cd5e35aa92913ee8
cjufi <ftasic1202@gmail.com>
2023-05-18 10:22:13 +0200

    Put project up to date v2

--- src/main/java/com/prime/rushhour/RushhourApplication.java
+++ src/main/java/com/prime/rushhour/RushhourApplication.java
@@ -1,11 +1,8 @@
 package com.prime.rushhour;
 
-import com.prime.rushhour.infrastructure.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RushhourApplication {
 
