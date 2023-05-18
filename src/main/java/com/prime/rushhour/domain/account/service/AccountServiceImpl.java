6faa61dae6217b18b9252850cb0341ff04ded4fc
cjufi <ftasic1202@gmail.com>
2023-05-18 10:15:16 +0200

    Update account domain

--- src/main/java/com/prime/rushhour/domain/account/service/AccountServiceImpl.java
+++ src/main/java/com/prime/rushhour/domain/account/service/AccountServiceImpl.java
@@ -1,9 +1,12 @@
 package com.prime.rushhour.domain.account.service;
 
 import com.prime.rushhour.domain.account.dto.AccountRequest;
+import com.prime.rushhour.domain.account.entity.Account;
 import com.prime.rushhour.domain.account.repository.AccountRepository;
 import com.prime.rushhour.infrastructure.exceptions.DuplicateResourceException;
+import com.prime.rushhour.infrastructure.exceptions.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AccountServiceImpl implements AccountService{
@@ -28,5 +31,11 @@ public class AccountServiceImpl implements AccountService{
         if(checkEmail(accountRequest.email())){
             throw new DuplicateResourceException("Email", accountRequest.email());
         }
+    }
+
+    @Override
+    public Account findByEmail(String email) {
+        return accountRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(Account.class.getSimpleName(), "email", email));
     }
 }
