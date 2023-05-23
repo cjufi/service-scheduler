608183210360a0ed0492c73d0568f9b2bb249dd2
cjufi <ftasic1202@gmail.com>
2023-05-23 12:06:02 +0200

    Add flyway migration for Activity

---
+++ src/main/resources/db/migration/V6__Add_activity.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS activity
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    price       DOUBLE       NOT NULL,
+    duration    DECIMAL      NOT NULL,
+    provider_id BIGINT       NOT NULL,
+    constraint fk_activity_provider
+        FOREIGN KEY (provider_id) references provider (id)
+);
+
+CREATE TABLE IF NOT EXISTS activity_employees
+(
+    activity_id BIGINT NOT NULL,
+    employee_id BIGINT NOT NULL,
+    constraint fk_activity_id
+        FOREIGN KEY (activity_id) references activity (id) ON DELETE CASCADE,
+    constraint fk_employee_id
+        FOREIGN KEY (employee_id) references employee (id) ON DELETE CASCADE
+);
