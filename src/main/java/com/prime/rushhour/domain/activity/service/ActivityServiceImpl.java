669e04df2854edabb05720311a962f39ff39c00d
cjufi <ftasic1202@gmail.com>
2023-05-25 10:26:56 +0200

    Implement Activity Service

--- src/main/java/com/prime/rushhour/domain/activity/service/ActivityServiceImpl.java
+++ src/main/java/com/prime/rushhour/domain/activity/service/ActivityServiceImpl.java
@@ -2,8 +2,10 @@ package com.prime.rushhour.domain.activity.service;
 
 import com.prime.rushhour.domain.activity.dto.ActivityRequest;
 import com.prime.rushhour.domain.activity.dto.ActivityResponse;
+import com.prime.rushhour.domain.activity.entity.Activity;
 import com.prime.rushhour.domain.activity.mapper.ActivityMapper;
 import com.prime.rushhour.domain.activity.repository.ActivityRepository;
+import com.prime.rushhour.infrastructure.exceptions.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,26 +24,37 @@ public class ActivityServiceImpl implements ActivityService {
 
     @Override
     public ActivityResponse save(ActivityRequest activityRequest) {
-        return null;
+
+        var activity = activityMapper.toEntity(activityRequest);
+        return activityMapper.toDto(activityRepository.save(activity));
     }
 
     @Override
     public ActivityResponse getById(Long id) {
-        return null;
+        var activity = activityRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException(Activity.class.getSimpleName(), "id", id));
+        return activityMapper.toDto(activity);
     }
 
     @Override
     public Page<ActivityResponse> getAll(Pageable pageable) {
-        return null;
+        return activityRepository.findAll(pageable).map(activityMapper::toDto);
     }
 
     @Override
     public void delete(Long id) {
-
+        if(!activityRepository.existsById(id)){
+            throw new EntityNotFoundException(Activity.class.getSimpleName(), "id", id);
+        }
+        activityRepository.deleteById(id);
     }
 
     @Override
     public ActivityResponse update(Long id, ActivityRequest activityRequest) {
-        return null;
+        var activity = activityRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(Activity.class.getSimpleName(), "id", id));
+
+        activityMapper.update(activity, activityRequest);
+        return activityMapper.toDto(activityRepository.save(activity));
     }
 }
