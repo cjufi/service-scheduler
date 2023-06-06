526c70a7026d03d8b983daf8f36daaea19a830b3
cjufi <ftasic39@gmail.com>
2023-06-06 11:47:29 +0200

    Add Appointment Service Implementation

--- src/main/java/com/prime/rushhour/domain/appointment/service/AppointmentServiceImpl.java
+++ src/main/java/com/prime/rushhour/domain/appointment/service/AppointmentServiceImpl.java
@@ -2,7 +2,10 @@ package com.prime.rushhour.domain.appointment.service;
 
 import com.prime.rushhour.domain.appointment.dto.AppointmentRequest;
 import com.prime.rushhour.domain.appointment.dto.AppointmentResponse;
+import com.prime.rushhour.domain.appointment.entity.Appointment;
+import com.prime.rushhour.domain.appointment.mapper.AppointmentMapper;
 import com.prime.rushhour.domain.appointment.repository.AppointmentRepository;
+import com.prime.rushhour.infrastructure.exceptions.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,32 +15,47 @@ public class AppointmentServiceImpl implements AppointmentService{
 
     private final AppointmentRepository appointmentRepository;
 
-    public AppointmentServiceImpl(AppointmentRepository appointmentRepository) {
+    private final AppointmentMapper appointmentMapper;
+
+    public AppointmentServiceImpl(AppointmentRepository appointmentRepository, AppointmentMapper appointmentMapper) {
         this.appointmentRepository = appointmentRepository;
+        this.appointmentMapper = appointmentMapper;
     }
 
     @Override
     public AppointmentResponse save(AppointmentRequest appointmentRequest) {
-        return null;
+        var appointment = appointmentMapper.toEntity(appointmentRequest);
+        appointment.setEndDate(appointment.getStartDate().plusMinutes(appointment.getActivity().getDuration().toMinutes()));
+        return appointmentMapper.toDto(appointmentRepository.save(appointment));
     }
 
     @Override
     public AppointmentResponse getById(Long id) {
-        return null;
+        var appointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Appointment.class.getSimpleName(), "id", id));
+        return appointmentMapper.toDto(appointment);
     }
 
     @Override
     public Page<AppointmentResponse> getAll(Pageable pageable) {
-        return null;
+        return appointmentRepository.findAll(pageable).map(appointmentMapper::toDto);
     }
 
     @Override
     public void delete(Long id) {
-
+        if(!appointmentRepository.existsById(id)) {
+            throw new EntityNotFoundException(Appointment.class.getSimpleName(), "id", id);
+        }
+        appointmentRepository.deleteById(id);
     }
 
     @Override
     public AppointmentResponse update(Long id, AppointmentRequest appointmentRequest) {
-        return null;
+        var appointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Appointment.class.getSimpleName(), "id", id));
+
+        appointment.setEndDate(appointment.getStartDate().plusMinutes(appointment.getActivity().getDuration().toMinutes()));
+        appointmentMapper.update(appointment, appointmentRequest);
+        return appointmentMapper.toDto(appointmentRepository.save(appointment));
     }
 }
