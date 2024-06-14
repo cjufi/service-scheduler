79f0ac70c9f9321b1f9a1de0af7fecb9f3633636
cjufi <ftasic1202@gmail.com>
2024-06-14 19:29:38 +0200

    Add role docs

--- src/main/java/com/prime/rushhour/domain/role/entity/Role.java
+++ src/main/java/com/prime/rushhour/domain/role/entity/Role.java
@@ -2,36 +2,78 @@ package com.prime.rushhour.domain.role.entity;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents a role in the system.
+ *
+ * A role has a unique identifier and a name.
+ *
+ * @author Filip
+ * @version 1.0
+ */
 @Entity
 public class Role {
 
+    /**
+     * Unique identifier for the role.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
+    /**
+     * Name of the role, must be unique and not null.
+     */
     @Column(nullable = false, unique = true)
     private String name;
 
+    /**
+     * Creates a new role with uninitialized fields.
+     */
     public Role() {
     }
 
+    /**
+     * Creates a new role with the specified name.
+     *
+     * @param name name of the role
+     */
     public Role(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the unique identifier of the role.
+     *
+     * @return id of the role
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the role.
+     *
+     * @param id id of the role
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Returns the name of the role.
+     *
+     * @return name of the role
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the role.
+     *
+     * @param name name of the role
+     */
     public void setName(String name) {
         this.name = name;
     }
